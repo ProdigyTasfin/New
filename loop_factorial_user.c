56c8fbcc04e9f15b9cf04e52f40fe49ecfd2d699
Tas fin <151371523+ProdigyTasfin@users.noreply.github.com>
2025-06-03 16:19:32 +0000

    Initial commit of my VS Code project

--- loop_factorial_user.c
+++ loop_factorial_user.c
@@ -4,14 +4,21 @@
 
 int main() {
     int n;
+    int i =1;
     int fact = 1;  // Move this outside the loop
 
     printf("Enter your number: ");
     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++) {
-        fact = fact * i;
+    // for (int i = 1; i <= n; i++) {
+    //     fact = fact * i;
+    // }
+
+    while (i <=n)
+    {   fact = fact * i;
+        i++;
     }
+    
 
     printf("Factorial of your given number (%d) is: %d\n", n, fact);
 
